ab4beb5d9bc89a375573395297d6b33ee7e779be
RAJAN13-blip <76870869+RAJAN13-blip@users.noreply.github.com>
2021-11-17 13:36:19 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+package com.company;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    static LogFile log_file = new LogFile();
 
     public static void go_to_login_page()throws Exception{
 
@@ -15,13 +18,13 @@ public class Main {
             main(arg1);
 
         }
-        
+
         else if(read.equals("Q")){
 
             System.exit(0);
 
         }
-        
+
         else{
 
             System.out.println("Please give a valid input !");
@@ -35,6 +38,7 @@ public class Main {
 
         // Login welcome = new Login();
         // String[] arg1 = new String[2];
+        log_file.append("Program Started");
 
         System.out.println("*********************************************************************************");
         System.out.println("WELCOME TO AIRLINE RESERVATION SYSTEM !\n");
@@ -44,63 +48,78 @@ public class Main {
         String read = reader.readLine();
 
         if(read.equals("L")){
-
-              Customer c = Login.LOG_IN();
-//            c.check_balance();
-//            System.out.println(c.username);
-//            System.out.println(c.Name);
-//            System.out.println(c.check_balance());
-
-            System.out.println("Welcome Mr./Mrs."+c.Name);
-            System.out.println("------------------------");
-            System.out.println("------------------------");
-            System.out.println("Press C to change credentials U to update balance: ");
-            // System.out.print("||Press U to update balance");
+            System.out.println("Login as a Customer(C) or Login as a Staff-Member(S)");
             read = reader.readLine();
 
-            if(read.equals("C")){
+            if(read.equals("C")) {
+                Customer c = Login.LOG_IN();
 
-                c.change_credentials();
-                System.out.println("Do you want to quit? Type Y for yes and N for no: ");
+                System.out.println("Welcome Mr./Mrs." + c.Name);
+                System.out.println("------------------------");
+                System.out.println("------------------------");
+                System.out.println("Press C to change credentials U to update balance: ");
+                // System.out.print("||Press U to update balance");
                 read = reader.readLine();
 
-                if(read.equals("N"))
-                    c = Login.LOG_IN();
+                if (read.equals("C")) {
 
-                else if(read.equals("Y"))
-                    System.exit(0);
+                    c.change_credentials();
+                    System.out.println("Do you want to quit? Type Y for yes and N for no: ");
+                    read = reader.readLine();
 
-                else{
+                    if (read.equals("N"))
+                        c = Login.LOG_IN();
 
-                    System.out.println("Please give a valid input !");
-                    go_to_login_page();
+                    else if (read.equals("Y")) {
+                        log_file.append(c, "User exited,Program exited");
+                        System.exit(0);
+                    } else {
 
+                        System.out.println("Please give a valid input !");
+                        go_to_login_page();
+
+                    }
+
+                } else if (read.equals("U")) {
+                    c.update_balance();
                 }
-
-            }
-            
-            else if(read.equals("U")){
-                c.update_balance();
+            }else if(read.equals("S")){
+                Staff s = Login.sLOG_IN();
+            }else{
+                System.out.println("invalid input");
+                go_to_login_page();
             }
 
 
 
         }
-        
+
         else if(read.equals("S")){
+            System.out.println("Sign up as a Customer(C), or sign up as a Staff(S)");
+            if(read.equals("C")) {
+                Login.SIGN_UP("Customer");
+                System.out.println("User account set up done !");
+                go_to_login_page();
+            }
+            else if(read.equals("S")){
+                Login.SIGN_UP("Staff");
+                System.out.println("User account set up done !");
+                go_to_login_page();
+            }else{
+                System.out.println("Invalid input");
+                go_to_login_page();
+            }
 
-            Login.SIGN_UP();
-            System.out.println("User account set up done !");
-            go_to_login_page();
-            
         }
-        
+
         else if(read.equals("Q")){
 
+            log_file.append("Program terminated");
             System.exit(0);
 
+
         }
-        
+
         else{
 
             System.out.println("Please give a valid input !");
@@ -111,4 +130,3 @@ public class Main {
 
     }
 }
-
