5078109fc78e00d491b6beb5c59b518b1085d3f1
RAJAN13-blip <76870869+RAJAN13-blip@users.noreply.github.com>
2021-11-25 23:03:14 +0530

    Update City.java

--- City.java
+++ City.java
@@ -11,28 +11,23 @@ import java.lang.Math;
 public class City {
 
     static String current_dir = System.getProperty("user.dir");
-    final static String Home_city = "Delhi"; // Home city
+    final static String Home_city = "Delhi";
     private String name;
     private String airport;
     private Point coordinates;
 
-    // Default constructor
     City(){
-
-        // All members are read-only
         this.name = this.Home_city;
-        this.coordinates = new Point();
+        this.coordinates = new Point(0,0);
         this.airport = "DEL";
     }
 
-    // Parameterized constructor
     City(String name, String airport, Point point){
         this.name = name;
         this.airport = airport;
         this.coordinates = point;
     }
 
-    // Objects of the 10 cities as members to be further used for an external object of class
     static City c1 = new City(); // For Delhi
     static City c2 = new City("Mumbai", "BOM", new Point(-2000,-4000));
     static City c3 = new City("Kolkata", "KOL", new Point(2000,-3000));
@@ -45,7 +40,6 @@ public class City {
     static City c10 = new City("Chandigarh", "IXE", new Point(-200,0));
 
 
-    // Map that has: key - City name, value - Coordinates (home city at origin)
     static Map<Object, Object> cities = Stream.of(new Object[][] {
             { c1, c1.coordinates },
             { c2, c2.coordinates },
@@ -61,25 +55,34 @@ public class City {
             Collectors.toMap(data -> data[0], data -> data[1]),
             Collections::<Object, Object> unmodifiableMap));
 
-    //--------------------------------------------Getters and setters---------------------------------------------//
-            // Get city name
     public String getName() {
         return this.name;
     }
 
-    // Get airport code of the city
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getAirport() {
         return this.airport;
     }
 
-    // Get location of the city
+    public void setAirport(String airport) {
+        this.airport = airport;
+    }
+
     public Point getCoordinates() {
         return this.coordinates;
     }
-    //------------------------------------------------------------------------------------------------------------//
 
-    // Calculates and returns the distance between a given pair of cities
-    static int cal_dist(String from, String to){
+    public void setCoordinates(Point coordinates) {
+        this.coordinates = coordinates;
+    }
+
+
+
+
+    int cal_dist(String from, String to){
         Point p1 = (Point) cities.get(from);
         Point p2 = (Point) cities.get(to);
 
@@ -88,44 +91,8 @@ public class City {
         return dist;
     }
 
-    // Creates cities.txt file
-    // Currently cities.txt has content of all 10 cities and distance between each other (45 rows)
-    // To run this, clear exisiting contents of cities.txt, add a driver class with main function
-    // and call this function in a static way (City.construct file)
-    static void construct_file(){
-
-        PrintWriter pw = null;
-
-        try{
-            pw = new PrintWriter(new FileWriter("cities.txt", true));
-
-            Set<Object> keys = cities.keySet();
-            String[] arr = new String[10];
-            int i = 0;
-            for(Object s:keys){
-                arr[i] = s.toString();
-                i++;
-            }
-
-            for(int k=0;k<arr.length;k++){
-                for (int j=k+1;j<arr.length;j++ ){
-                    pw.write(arr[k]+","+arr[j]+","+Integer.toString(cal_dist(arr[k], arr[j]))+"\n");
-                }
-            }
-
-            pw.flush();
-            pw.close();
-
-            System.out.println("Cities file created!");
-        }
-        catch(Exception e){
-            System.out.println(e.toString());
-        }
 
 
-    }
-
-    // Displays the list of cities from and to each city in a formatted way
     public static void display_list_of_cities(){
 
         BufferedReader br = null;
@@ -142,8 +109,6 @@ public class City {
             System.out.println("-----------------------------------------");
             System.out.println("From              To");
             System.out.println("-----------------------------------------");
-
-            // Read file
             while((str = br.readLine()) != null){
                 display = str.split(",");
                 String space = "";
@@ -163,7 +128,6 @@ public class City {
         }
     }
 
-    // Returns the city object while booking to extract airport code etc.
     public City assign_city(String city_name){
 
         ArrayList<City> city = new ArrayList<>();
@@ -183,36 +147,26 @@ public class City {
                 return c;
         }
 
-        return c1; // If city name is not valid then returns home city
+        return c1;
     }
-
 }
 
-// Class for handling city locations in terms of coordinates
 class Point extends Object{
 
     int x;
     int y;
 
 
-    // Default constructor
-    public Point() {
-
-        this.x = 0;
-        this.y = 0;
-    }
-    
-    // Parameterized constructor
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    // Prints the x and y coordinates of the point object
     void get_coord(){
         System.out.println(Integer.toString(this.x)+ "," + Integer.toString(this.y));
     }
 
 }
+
 
 
