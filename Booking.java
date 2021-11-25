5f0910fbc777eb4c4a8b41b12abca90eb5df2272
RAJAN13-blip <76870869+RAJAN13-blip@users.noreply.github.com>
2021-11-25 23:02:11 +0530

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -10,22 +10,17 @@ public class Booking {
     private String to;
     private String seat_no = null;
 
-    // For the logs
-    static LogFile log_file = new LogFile();
 
-    // Default constructor
     public Booking() {
     }
 
 
-    // Parameterized constructor with just date, from and to (called just before booking)
     public Booking(String date, String from, String to) {
         this.date = date;
         this.from = from;
         this.to = to;
     }
 
-    // Parameterized constructor with date, time, from, to and seat number (called just before cancellation)
     public Booking(String date, String time, String from, String to, String seat) {
         this.date = date;
         this.from = from;
@@ -36,99 +31,74 @@ public class Booking {
 
     //--------------------------------------------Getters and setters---------------------------------------------//
 
-    // Get travel date of the booking
     public String getDate() {
         return this.date;
     }
 
-    // Assign travel date of the booking
     public void setDate(String date) {
         this.date = date;
     }
 
-    // Get travel time of the booking
     public String getTime() {
         return this.time;
     }
 
-    // Set travel time of the booking
     public void setTime(String time) {
         this.time = time;
     }
 
-    // Get airline of the booking
     public Airline getAirline() {
         return this.airline;
     }
 
-    // Get travel from of the booking
+//    public void setAirline(Airline airline) {
+//        this.airline = airline;
+//    }
+
     public String getFrom() {
         return this.from;
     }
 
-    // Assign travel from of the booking
     public void setFrom(String from) {
         this.from = from;
     }
 
-    // Get travel to of the booking
     public String getTo() {
         return this.to;
     }
 
-    // Assign travel to of the booking
     public void setTo(String to) {
         this.to = to;
     }
 
-    // Get seat number of the booking
     public String getseat_no() {
         return this.seat_no;
     }
 
-    // Get seat number of the booking
     public void setseat_no(String seat_no) {
         this.seat_no = seat_no;
     }
 
-    // All operations (namely, booking and cancellation) handled here
     public void seat_ops(String flightcode, String op, Customer c, TreeMap<String, String> passengers)throws Exception{
 
         Seats s = new Seats(flightcode);
 
-        // If operations is booking, then op = book
         if(op.equals("book")){
 
-            // TreeMap contains: key - Name of passenger, value - Age of passenger
             for (Map.Entry<String, String> entry : passengers.entrySet()) {
-
-                
-                s.book_seat(c, entry.getKey());
-
-                // Updates log file
-                log_file.append(c, "Ticket booked by " + c.username);
+                 s.book_seat(c, entry.getKey());
 
             }
-
-            System.out.println("Booking successful! Thank you!");
-            
+            System.out.println("Thank you for booking, Booking Successful !");
 
 
         }
 
-        // If operations is cancellation, then op = cancel
         else if(op.equals("cancel")){
-
-            // TreeMap contains: key - Name of passenger, value - Seat number of passenger
-            for (Map.Entry<String, String> entry : passengers.entrySet()){
-
+            for (Map.Entry<String, String> entry : passengers.entrySet())
                 s.cancel_seat(c, entry.getKey(), entry.getValue());
 
-                // Updates log file
-                log_file.append(c, "Ticket cancelled by " + c.username);
-            }
-
-            System.out.println("Cancellation successful! Your money has been refunded successfully!");
+            System.out.println("Thank you, your cancellation request has been processed.");
         }
 
 
